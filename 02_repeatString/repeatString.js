86f7cf5e48542af665b5771f276f4d1fb44c842e
Igor Golovko <2003igor09@gmail.com>
2023-11-03 22:33:11 +0200

    Complete first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times < 0) return "ERROR";
+    let str = '';
+    for(let i=0; i<times; i++){
+        str += word;
+    }
+    return str;
 };
 
 // Do not edit below this line
